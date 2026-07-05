17171b5b52136bfe4fa074f46b7351449ec1ed62
yousef-karem <yousefkarem91@gmail.com>
2026-07-06 01:50:20 +0300

    Add fix 3al4an nour yahda

--- src/main/java/ds/Max.java
+++ src/main/java/ds/Max.java
@@ -2,7 +2,8 @@ package ds;
 public class Max { 
   public static Item max (Item v[], int n) { 
     MeuItem max = (MeuItem) v[0];
-    for (int i = 1; i < n; i++) if (max.checkIfAnimalIsCat()) max = (MeuItem) v[i];
+    for (int i = 1; i < n; i++) 
+      if (max.checkIfAnimalIsCat()) max = (MeuItem) v[i];
     return max;
   }
 }
